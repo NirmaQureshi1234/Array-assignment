df199202fbb5eac32bfb95d44376e0e7788a0b40
NirmaQureshi1234 <nirmaqureshi786@gmail.com>
2024-05-22 20:58:59 -0700

    Add files via upload

---
+++ Array1 assignment/array.ts
@@ -0,0 +1,78 @@
+                 //ARRAY ASSIGNMENT 
+//ans1
+// Create an array named fruits that contains the following string
+ //elements: "apple", "banana", "mango", "orange".
+//const fruits = ["apple", "banana", "mango", "orange"];
+//console.log(fruits,"fruits");
+
+//ans2
+// Declare an array named numbers that can contain only number  
+//elements and initialize it with the values10, 20, 30, and 40.  
+//const numbers = [10,20,30,40];
+//console.log(numbers,"numbers");
+
+//ans3
+//Access the third element of the fruits array and assign it to
+// a  variable named thirdFruit.  
+// let fruit: string[] = ["pineapple", "strawberry", "watermelon", "orange"];
+ //let thirdFruit: string = fruit[2]; 
+ //console.log(thirdFruit); 
+
+//ans4
+//change the second element of the numbers array to 25.
+// let numbers:number[] = [10,20,30,40];
+ //numbers[1] = 25;
+ //console.log(numbers);
+
+//ans5
+//Add the element "grape" to the end of the fruits array using the  method.
+//let fruit: string[] = ["apple","banana","mango","orange"];
+// fruit.push("grape");
+//console.log(fruit,"fruit");
+
+//ans6
+//Remove the last element from the fruits array using the  method
+// and assign it to a variable named lastFruit. 
+ //let lastfruit: string[] = ["apple","banana","mango","orange"];
+//lastfruit.pop();
+ //console.log(lastfruit,"lastfruit");
+
+//ans7
+//Remove the first element from the fruits array using the  method
+// and assign it to a variable named firstFruit. 
+ //let firstfruit: string[] = ["watermelon","pomegranet","pineapple","grapes"];
+//firstfruit.shift();
+//console.log(firstfruit,"firstfruit");
+
+//ans8
+//Add the element "kiwi" to the beginning of the fruits array  using the method. 
+//let fruits: string[] = ["apple","banana","mango","orange"];
+ //.unshift("kiwi");
+ //console.log(fruits,"fruit");
+
+//ans 9
+//Remove 2 elements from the fruits array starting from index 1  using the method.  
+//let fruits: string[] = ["apple","banana","mango","orange"];
+//fruits.splice(1,2);
+//console.log(fruits); 
+
+//ans 10
+//Insert the elements "pineapple" and "pear" at index 2 of the 
+// fruits array using the method. 
+//let fruits:string[] = ["apple","banana","mango","orange"];
+ //fruits.splice (2,0, "pineapple","pear");
+// console.log(fruits);
+
+//ans 11
+//Create a new array named citrusFruits that contains the first 
+// two elements of the fruits array using the method.  
+// let fruit: string[] = ["apple","banana","mango","orange"];
+ //let citrousfruit: string[] =fruit.splice(0,2);
+// console.log(citrousfruit);
+
+//ans 12
+// Create a new array named lastTwoFruits that contains the last 
+//  two elements of the fruits array using the method. 
+ //let fruits: string[] = ["apple","banana","mango","orange"];
+//let lastTwoFruits: string [] = fruits.slice(-2);
+//console.log(lastTwoFruits);
